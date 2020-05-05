c70188719dadac4ee48932a8fe03d000257d2a8a
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-05-05 22:40:30 +0530

    Init

---
+++ backend/src/index.js
@@ -0,0 +1,22 @@
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+server.express.use(cookieParser());
+
+server.start(
+	{
+		cors: {
+			credentials: true,
+			origin: process.env.FRONTEND_URL
+		}
+	},
+	(deets) => {
+		console.log(`Server is now running on port http://localhost:${deets.port}`);
+	}
+);
