22c3e77908894c186f2421bc094dcc1a8edfba13
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-05-06 11:04:42 +0530

    story table created

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -124,6 +124,24 @@ const Mutations = {
 		if (!ctx.request.userId) {
 			throw new Error('You must be logged in to do that!');
 		}
+		const item = await ctx.db.mutation.createItem(
+			{
+				data: {
+					// This is how to create a relationship between the Item and the User
+					user: {
+						connect: {
+							id: ctx.request.userId
+						}
+					},
+					...args
+				}
+			},
+			info
+		);
+
+		//console.log(item);
+
+		return item;
 	}
 };
 module.exports = Mutations;
