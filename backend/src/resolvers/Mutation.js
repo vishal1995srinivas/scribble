1bcc18afd1a789d084f900db3a8ce24c674c9dfe
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-05-06 10:54:07 +0530

    reset password

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -82,6 +82,48 @@ const Mutations = {
 
 		// 4. Return the message
 		return { message: 'Thanks!' };
+	},
+	async resetPassword(parent, args, ctx, info) {
+		// 1. check if the passwords match
+		if (args.password !== args.confirmPassword) {
+			throw new Error("Yo Passwords don't match!");
+		}
+		// 2. check if its a legit reset token
+		// 3. Check if its expired
+		const [ user ] = await ctx.db.query.users({
+			where: {
+				resetToken: args.resetToken,
+				resetTokenExpiry_gte: Date.now() - 3600000
+			}
+		});
+		if (!user) {
+			throw new Error('This token is either invalid or expired!');
+		}
+		// 4. Hash their new password
+		const password = await bcrypt.hash(args.password, 10);
+		// 5. Save the new password to the user and remove old resetToken fields
+		const updatedUser = await ctx.db.mutation.updateUser({
+			where: { email: user.email },
+			data: {
+				password,
+				resetToken: null,
+				resetTokenExpiry: null
+			}
+		});
+		// 6. Generate JWT
+		const token = jwt.sign({ userId: updatedUser.id }, process.env.APP_SECRET);
+		// 7. Set the JWT cookie
+		ctx.response.cookie('token', token, {
+			httpOnly: true,
+			maxAge: 1000 * 60 * 60 * 24 * 365
+		});
+		// 8. return the new user
+		return updatedUser;
+	},
+	async createStory(parent, args, ctx, info) {
+		if (!ctx.request.userId) {
+			throw new Error('You must be logged in to do that!');
+		}
 	}
 };
 module.exports = Mutations;
