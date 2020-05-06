0f78bc43acc6958a47eac7c7db4dc93ac5f7dc7b
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-05-07 01:14:30 +0530

    total likes query added

--- frontend/components/TotalLikes.js
+++ frontend/components/TotalLikes.js
@@ -2,10 +2,29 @@ import React from 'react';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 
+const TOTAL_LIKES_QUERY = gql`
+	query TOTAL_LIKES_QUERY {
+		likesesConnection {
+			aggregate {
+				count
+			}
+		}
+	}
+`;
 const TotalLikes = (props) => (
-	<div>
-		{count} like{count > 1 && 's'}
-	</div>
+	<Query query={TOTAL_LIKES_QUERY}>
+		{({ data, loading, error }) => {
+			if (loading) return <p>Loading...</p>;
+			if (error) return <p>Error</p>;
+			const count = data.likesesConnection.aggregate.count;
+			console.log(count);
+			return (
+				<div>
+					{count} like{count > 1 && 's'}
+				</div>
+			);
+		}}
+	</Query>
 );
 export default TotalLikes;
 export { TOTAL_LIKES_QUERY };
