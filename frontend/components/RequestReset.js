30ba79b79133e3a923e1f369f3c6ab7f25fd1c98
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-05-06 21:59:20 +0530

    request reset mutation added

--- frontend/components/RequestReset.js
+++ frontend/components/RequestReset.js
@@ -4,6 +4,13 @@ import gql from 'graphql-tag';
 import Form from './styles/Form';
 import Error from './ErrorMessage';
 
+const REQUEST_RESET_MUTATION = gql`
+	mutation REQUEST_RESET_MUTATION($email: String!) {
+		requestReset(email: $email) {
+			message
+		}
+	}
+`;
 class RequestReset extends Component {
 	state = {
 		email: ''
@@ -13,32 +20,37 @@ class RequestReset extends Component {
 	};
 	render() {
 		return (
-			<Form
-				method="post"
-				data-test="form"
-				onSubmit={async (e) => {
-					e.preventDefault();
-					await reset();
-					this.setState({ email: '' });
-				}}
-			>
-				<fieldset disabled={loading} aria-busy={loading}>
-					<h2>Request a password reset</h2>
-					<Error error={error} />
-					{!error && !loading && called && <p>Success! Check your email for a reset link!</p>}
-					<label htmlFor="email">
-						Email
-						<input
-							type="email"
-							name="email"
-							placeholder="email"
-							value={this.state.email}
-							onChange={this.saveToState}
-						/>
-					</label>
-					<button type="submit">Request Reset</button>
-				</fieldset>
-			</Form>
+			<Mutation mutation={REQUEST_RESET_MUTATION} variables={this.state}>
+				{(reset, { error, loading, called }) => (
+					<Form
+						method="post"
+						data-test="form"
+						onSubmit={async (e) => {
+							e.preventDefault();
+							await reset();
+							this.setState({ email: '' });
+						}}
+					>
+						<fieldset disabled={loading} aria-busy={loading}>
+							<h2>Request a password reset</h2>
+							<Error error={error} />
+							{!error && !loading && called && <p>Success! Check your email for a reset link!</p>}
+							<label htmlFor="email">
+								Email
+								<input
+									type="email"
+									name="email"
+									placeholder="email"
+									value={this.state.email}
+									onChange={this.saveToState}
+								/>
+							</label>
+
+							<button type="submit">Request Reset</button>
+						</fieldset>
+					</Form>
+				)}
+			</Mutation>
 		);
 	}
 }
