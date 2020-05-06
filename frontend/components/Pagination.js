94f60906b0e8e226e44f95bc231de88b07debb6f
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-05-06 19:42:36 +0530

    Pagination

--- frontend/components/Pagination.js
+++ frontend/components/Pagination.js
@@ -5,8 +5,61 @@ import { Query } from 'react-apollo';
 import { perPage } from '../config';
 import Head from 'next/head';
 import Link from 'next/link';
+const PAGINATION_QUERY = gql`
+	query PAGINATION_QUERY {
+		postsConnection {
+			aggregate {
+				count
+			}
+		}
+	}
+`;
+const Pagination = (props) => (
+	<Query query={PAGINATION_QUERY}>
+		{({ data, loading, error }) => {
+			if (loading) return <p>Loading...</p>;
+			if (error) return <p>Error</p>;
 
-const Pagination = (props) => <div />;
+			const count = data.postsConnection.aggregate.count;
+			const pages = Math.ceil(count / perPage);
+			return (
+				<PaginationStyles data-test="pagination">
+					<Head>
+						<title>
+							Scribble -- {props.page} of {pages}
+						</title>
+					</Head>
+					<Link
+						prefetch
+						href={{
+							pathname: 'Posts',
+							query: { page: props.page - 1 }
+						}}
+					>
+						<a className="prev" aria-disabled={props.page <= 1}>
+							⬅ Prev
+						</a>
+					</Link>
+					<p>
+						Page {props.page} of <span className="totalPages"> {pages}</span>
+					</p>
+					<p>{count} Posts Total</p>
+					<Link
+						prefetch
+						href={{
+							pathname: 'Posts',
+							query: { page: props.page + 1 }
+						}}
+					>
+						<a className="next" aria-disabled={props.page >= pages}>
+							Next ➡
+						</a>
+					</Link>
+				</PaginationStyles>
+			);
+		}}
+	</Query>
+);
 
 export default Pagination;
-// export { PAGINATION_QUERY };
+export { PAGINATION_QUERY };
