2384754500663987c369a653eace3c81f48fba7f
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-05-07 13:41:40 +0530

    favicon added

--- frontend/components/Meta.js
+++ frontend/components/Meta.js
@@ -3,7 +3,7 @@ const Meta = () => (
 	<Head>
 		<meta name="viewport" content="width=device-width,initial-scale=1" />
 		<meta charSet="utf-8" />
-		<link rel="shortcut icon" href="/static/favicon.png" />
+		<link rel="shortcut icon" href="/static/favicon.ico" />
 		<link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
 		<title>Scribble</title>
 	</Head>
